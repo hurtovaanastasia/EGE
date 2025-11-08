171736f95d4e264fde51f055b00cb19938054b10
hurtovaanastasia <hurtovanasta85@gmail.com>
2025-11-08 09:04:30 +0300

    ыыыы

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,38 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String s = Files.readString(Paths.get("24_demo.txt"));
+    public static void main(String[] args) {
+        try {
+            String s = Files.readString(Paths.get("24_demo.txt"));
 
-        Pattern pat = Pattern.compile("(?:XYZ)+");
-        Matcher match = pat.matcher(s);
+            Pattern pattern = Pattern.compile("(?:XYZ)+");
+            Matcher matcher = pattern.matcher(s);
 
-        int maxLen = 0;
+            int maxLen = 0;
 
-        while (match.find()) {
-            String found = match.group();
+            while (matcher.find()) {
+                String found = matcher.group();
 
-            if (s.contains(found + "X")) {
-                found += "X";
-                if (s.contains(found + "Y")) {
-                    found += "Y";
+                // проверяем возможность добавить X и Y
+                if (s.contains(found + "X")) {
+                    found += "X";
+                    if (s.contains(found + "Y")) {
+                        found += "Y";
+                    }
+                }
+
+                if (found.length() > maxLen) {
+                    maxLen = found.length();
                 }
             }
 
-            if (found.length() > maxLen) {
-                maxLen = found.length();
-            }
-        }
+            System.out.println("Максимальная длина цепочки вида XYZXYZXYZ... = " + maxLen);
 
-        System.out.println("Максимальная длина цепочки вида XYZXYZXYZ... = " + maxLen);
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+            System.out.println("Проверьте, что файл существует и его путь и название  указаны правильно.");
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так: " + e.getMessage());
+        }
     }
 }
